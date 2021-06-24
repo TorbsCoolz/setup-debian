eb8c75750190f9ab15d7bb5077d54e5f8d18c457
TorbsCoolz <74032381+TorbsCoolz@users.noreply.github.com>
2021-06-24 02:45:13 +0000

    Update setup-debian.sh

--- setup-debian.sh
+++ setup-debian.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 apt update -y
 apt upgrade -y
-apt install firefox-esr tor wget nano xfce4 xfce4-goodies xfce4-whiskermenu-plugin gtk-nocsd marco tar zip unzip -y
+apt install firefox-esr firefox tor wget nano xfce4 xfce4-goodies xfce4-whiskermenu-plugin lightdm gtk-nocsd marco tar zip unzip -y
 wget https://codeload.github.com/matthewmx86/Redmond97/zip/refs/heads/master
 unzip master -d main
 cp -r main/Redmond97-master/Theme/csd/Redmond97 /usr/share/themes
@@ -10,4 +10,4 @@ cp -r "main/Redmond97-master/Theme/csd/Redmond97 Millennium XP" /usr/share/theme
 cp -r main/Redmond97-master/Extras/Icons/Redmond97 /usr/share/icons
 gtk-update-icon-cache /usr/share/icons/Redmond97/
 cd ~/
-apt install gparted xfce4-taskmanager torbrowser-launcher libreoffice thunderbird gimp tilp2 cool-retro-term -y
+apt install gparted xfce4-taskmanager torbrowser-launcher libreoffice thunderbird gimp tilp2 cool-retro-term xterm xfonts-cyrillic transmission-gtk vlc htop clementine -y
